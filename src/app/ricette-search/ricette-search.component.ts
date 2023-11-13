aec15d34e7c73fd8fc2a30375b2a1b3dca9fec04
Alberto Gonella <albygonella@gmail.com>
2023-11-13 09:41:51 +0100

    iniziata ui

---
+++ src/app/ricette-search/ricette-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ricette-search',
+  templateUrl: './ricette-search.component.html',
+  styleUrls: ['./ricette-search.component.css']
+})
+export class RicetteSearchComponent {
+
+}
